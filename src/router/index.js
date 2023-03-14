28d08e5f66dd4778b311a097426f746f00e0a4f1
zhengwenbo <19802021891@139.com>
2023-03-14 19:05:13 +0800

    想法页搭建

--- src/router/index.js
+++ src/router/index.js
@@ -2,11 +2,13 @@ import {createRouter, createWebHashHistory} from 'vue-router'
 
 import HomePage from "@/components/HomePage";
 import FeedbackPage from "@/components/FeedbackPage"
+import ThinkingPage from "@/components/ThinkingPage"
 
 const routes = [
     {
         path: '/', component: HomePage, children: [
-            {path: 'feedback', component: FeedbackPage}
+            {path: 'feedback', component: FeedbackPage},
+            {path: 'thinking', component: ThinkingPage}
         ]
     }
 ]
