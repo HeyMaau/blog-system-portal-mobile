5f9f41c3d8b8fe0b26add4b89441af5a519954c0
zhengwenbo <19802021891@139.com>
2023-03-16 17:43:20 +0800

    获取文章列表数据抽取成hook

--- src/components/CategoryPage.vue
+++ src/components/CategoryPage.vue
@@ -4,38 +4,16 @@
 
 <script setup>
 import ArticleList from "@/components/ArticleList";
-import {getArticlesApi} from "@/hooks/article";
+import {useGetArticles} from "@/hooks/article";
 import {useRoute, onBeforeRouteUpdate} from 'vue-router'
-import {ARTICLE_SUMMARY_LENGTH, CODE_SUCCESS} from "@/utils/constants";
-import {shallowRef} from "vue";
 
 //获取文章数据
 const route = useRoute()
-let articleList = shallowRef([])
-getArticleList(route.params.id)
+let articleList = useGetArticles(1, 10, route.params.id)
 
-function getArticleList(categoryID) {
-  console.log(categoryID)
-  getArticlesApi(1, 10, categoryID).then(({data: response}) => {
-    if (response.code === CODE_SUCCESS) {
-      articleList.value = response.data.data
-      trimArticleSummary(articleList.value)
-    }
-  })
-}
-
-function trimArticleSummary(list) {
-  if (list !== undefined) {
-    list.forEach(item => {
-      if (item.content.length > ARTICLE_SUMMARY_LENGTH) {
-        item.content = item.content.slice(0, ARTICLE_SUMMARY_LENGTH) + '...'
-      }
-    })
-  }
-}
 
 onBeforeRouteUpdate((to) => {
-  getArticleList(to.params.id)
+  articleList = useGetArticles(1, 10, to.params.id)
 })
 
 </script>
--- src/hooks/article.js
+++ src/hooks/article.js
@@ -1,5 +1,6 @@
 import axios from "axios";
-import {API_PORTAL_PATH} from "@/utils/constants";
+import {API_PORTAL_PATH, ARTICLE_SUMMARY_LENGTH, CODE_SUCCESS} from "@/utils/constants";
+import {shallowRef} from "vue";
 
 const request = axios.create({
     baseURL: API_PORTAL_PATH,
@@ -18,4 +19,26 @@ export function getArticlesApi(page, size, categoryID) {
             categoryID
         }
     })
+}
+
+export function useGetArticles(page, size, categoryID) {
+    const list = shallowRef([])
+    getArticlesApi(page, size, categoryID).then(({data: response}) => {
+        if (response.code === CODE_SUCCESS) {
+            const articleList = response.data.data
+            trimArticleSummary(articleList)
+            list.value = articleList
+        }
+    })
+    return list
+}
+
+function trimArticleSummary(list) {
+    if (list !== undefined) {
+        list.forEach(item => {
+            if (item.content.length > ARTICLE_SUMMARY_LENGTH) {
+                item.content = item.content.slice(0, ARTICLE_SUMMARY_LENGTH) + '...'
+            }
+        })
+    }
 }
