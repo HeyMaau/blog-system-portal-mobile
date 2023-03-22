8507c2a5a2adb15a5a1ed0cc75941d13f6f59fb6
zhengwenbo <19802021891@139.com>
2023-03-22 14:37:58 +0800

    使用CDN优化Webpack打包，多环境入口

---
+++ src/main-prod.js
@@ -0,0 +1,43 @@
+import {createApp} from 'vue'
+import App from './App.vue'
+import router from '@/router/index'
+
+import {
+    NavBar,
+    Button,
+    Image as VanImage,
+    Search,
+    Icon,
+    Popup,
+    Collapse,
+    CollapseItem,
+    Cell,
+    Form,
+    Field,
+    CellGroup,
+    Toast,
+    BackTop,
+    Sticky
+} from 'vant';
+
+import '@/assets/css/global.css'
+
+const app = createApp(App)
+app.use(router).mount('#app')
+
+app.use(NavBar)
+app.use(Button)
+app.use(VanImage)
+app.use(Search)
+app.use(Icon)
+app.use(Popup)
+app.use(Collapse)
+app.use(CollapseItem)
+app.use(Cell)
+app.use(Form)
+app.use(Field)
+app.use(CellGroup)
+app.use(Toast)
+app.use(BackTop)
+app.use(Sticky)
+
--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,19 @@
-const { defineConfig } = require('@vue/cli-service')
+const {defineConfig} = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+    transpileDependencies: true,
+    configureWebpack: config => {
+        if (process.env.NODE_ENV === 'production') {
+            config.externals = {
+                'axios': 'axios',
+                'vue': 'Vue',
+                'vue-router': 'VueRouter',
+                'vant': 'vant'
+            }
+        }
+    },
+    pages: {
+        index: {
+            entry: process.env.NODE_ENV === 'production' ? 'src/main-prod.js' : 'src/main.js'
+        }
+    }
 })
