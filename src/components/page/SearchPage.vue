df4fae3b37be773dd8a68c5539c8dbb94a2e02ad
zhengwenbo <19802021891@139.com>
2023-04-10 15:23:06 +0800

    修复在搜索页面继续搜索时页面没有刷新的问题

--- src/components/page/SearchPage.vue
+++ src/components/page/SearchPage.vue
@@ -6,19 +6,32 @@
 import {doSearchApi} from "@/hooks/search";
 import {CODE_SUCCESS} from "@/utils/constants";
 import {ref} from "vue";
-import {useRoute} from "vue-router";
+import {useRoute, onBeforeRouteUpdate} from "vue-router";
 import {showFailToast} from "vant";
 import SearchList from "@/components/SearchList";
 
 const searchList = ref([])
 const route = useRoute()
 
-doSearchApi(route.query.keyword, 1, 5).then(({data: response}) => {
-  if (response.code === CODE_SUCCESS) {
-    searchList.value = response.data.data
-  } else {
-    showFailToast(response.message)
-  }
+let page = 1
+let size = 5
+
+doSearch(route.query.keyword, page, size)
+
+function doSearch(keyword, page, size) {
+  doSearchApi(keyword, page, size).then(({data: response}) => {
+    if (response.code === CODE_SUCCESS) {
+      searchList.value = response.data.data
+    } else {
+      showFailToast(response.message)
+    }
+  })
+}
+
+onBeforeRouteUpdate(to => {
+  page = 1
+  size = 5
+  doSearch(to.query.keyword, page, size)
 })
 
 </script>
