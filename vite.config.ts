91eaf47d2485597899841fd66548fff20f98a9e7
HeyMaau <841294180@163.com>
2025-05-27 23:48:33 +0800

    feat: markdown编辑器通过CDN引入

--- vite.config.ts
+++ vite.config.ts
@@ -30,14 +30,15 @@ export default defineConfig(({mode}) => {
                     input: {
                         main: resolve(__dirname, 'index-prod.html')
                     },
-                    externals: ['axios', 'vue', 'vue-router', 'vant', 'highlight.js'],
+                    externals: ['axios', 'vue', 'vue-router', 'vant', 'highlight.js', 'md-editor-v3'],
                     plugins: [
                         externalGlobals({
                             'axios': 'axios',
                             'vue': 'Vue',
                             'vue-router': 'VueRouter',
                             'vant': 'vant',
-                            'highlight.js': 'hljs'
+                            'highlight.js': 'hljs',
+                            'md-editor-v3': 'MdEditorV3'
                         })
                     ]
                 },
